8e4f054beee914837c1f1a920aa6c9520660219f
tprodanov <timofey.prodanov@gmail.com>
2016-02-18 00:36:40 +0300

    fixed style
    	modified:   src/main/java/ru/spbau/mit/StringSetImpl.java

--- src/main/java/ru/spbau/mit/StringSetImpl.java
+++ src/main/java/ru/spbau/mit/StringSetImpl.java
@@ -77,8 +77,9 @@ public class StringSetImpl implements StringSet {
             return false;
         }
         if (transitions[element.charAt(index) - 'A'].removeFromIndex(element, index + 1)) {
-            if (transitions[element.charAt(index) - 'A'].size() == 0)
+            if (transitions[element.charAt(index) - 'A'].size() == 0) {
                 transitions[element.charAt(index) - 'A'] = null;
+            }
             --size;
             return true;
         }

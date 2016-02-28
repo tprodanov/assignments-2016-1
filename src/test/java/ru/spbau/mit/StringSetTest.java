250d64d0ba65377931ad9e3efd584dd1a75b8744
Denis Zharkov <denis.zharkov@jetbrains.com>
2016-02-28 17:39:31 +0300

    Add task 02-serializable-string-set

--- src/test/java/ru/spbau/mit/StringSetTest.java
+++ src/test/java/ru/spbau/mit/StringSetTest.java
@@ -20,6 +20,42 @@ public class StringSetTest {
         assertEquals(1, stringSet.howManyStartsWithPrefix("abc"));
     }
 
+    @Test
+    public void testSimpleSerialization() {
+        StringSet stringSet = instance();
+
+        assertTrue(stringSet.add("abc"));
+        assertTrue(stringSet.add("cde"));
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ((StreamSerializable) stringSet).serialize(outputStream);
+
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+        StringSet newStringSet = instance();
+        ((StreamSerializable) newStringSet).deserialize(inputStream);
+
+        assertTrue(newStringSet.contains("abc"));
+        assertTrue(newStringSet.contains("cde"));
+    }
+
+
+    @Test(expected=SerializationException.class)
+    public void testSimpleSerializationFails() {
+        StringSet stringSet = instance();
+
+        assertTrue(stringSet.add("abc"));
+        assertTrue(stringSet.add("cde"));
+
+        OutputStream outputStream = new OutputStream() {
+            @Override
+            public void write(int b) throws IOException {
+                throw new IOException("Fail");
+            }
+        };
+
+        ((StreamSerializable) stringSet).serialize(outputStream);
+    }
+
     public static StringSet instance() {
         try {
             return (StringSet) Class.forName("ru.spbau.mit.StringSetImpl").newInstance();

dba01694c70cf29e06e9f3da4cc14d291f667011
Timofey Prodanov <timofey.prodanov@gmail.com>
2016-04-12 00:38:54 +0300

    4th home assignment

--- src/main/java/ru/spbau/mit/SecondPartTasks.java
+++ src/main/java/ru/spbau/mit/SecondPartTasks.java
@@ -1,7 +1,11 @@
 package ru.spbau.mit;
 
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public final class SecondPartTasks {
 
@@ -9,7 +13,14 @@ public final class SecondPartTasks {
 
     // Найти строки из переданных файлов, в которых встречается указанная подстрока.
     public static List<String> findQuotes(List<String> paths, CharSequence sequence) {
-        throw new UnsupportedOperationException();
+        return paths.stream().flatMap(filename -> {
+            try {
+                return Files.lines(Paths.get(filename));
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }).filter(line -> line.contains(sequence)).collect(Collectors.toList());
     }
 
     // В квадрат с длиной стороны 1 вписана мишень.
@@ -17,18 +28,27 @@ public final class SecondPartTasks {
     // Надо промоделировать этот процесс с помощью класса java.util.Random и посчитать,
     // какова вероятность попасть в мишень.
     public static double piDividedBy4() {
-        throw new UnsupportedOperationException();
+        final long tries = 10000;
+        Random random = new Random();
+        final double radius = .5;
+        long successCount = Stream.generate(() -> new double[] {random.nextDouble(), random.nextDouble()})
+            .limit(tries).filter(point -> Math.pow(point[0] - radius, 2) + Math.pow(point[1] - radius, 2)
+                        <= Math.pow(radius, 2)).count();
+        return successCount / (double) tries;
     }
 
     // Дано отображение из имени автора в список с содержанием его произведений.
     // Надо вычислить, чья общая длина произведений наибольшая.
     public static String findPrinter(Map<String, List<String>> compositions) {
-        throw new UnsupportedOperationException();
+        return compositions.entrySet().stream()
+                .max(Comparator.comparing(entry -> entry.getValue().stream().mapToInt(String::length).sum()))
+                .map(Map.Entry::getKey).orElse(null);
     }
 
     // Вы крупный поставщик продуктов. Каждая торговая сеть делает вам заказ в виде Map<Товар, Количество>.
     // Необходимо вычислить, какой товар и в каком количестве надо поставить.
     public static Map<String, Integer> calculateGlobalOrder(List<Map<String, Integer>> orders) {
-        throw new UnsupportedOperationException();
+        return orders.stream().flatMap(order -> order.entrySet().stream())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, Integer::sum));
     }
 }
--- src/test/java/ru/spbau/mit/SecondPartTasksTest.java
+++ src/test/java/ru/spbau/mit/SecondPartTasksTest.java
@@ -2,27 +2,84 @@ package ru.spbau.mit;
 
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class SecondPartTasksTest {
 
     @Test
     public void testFindQuotes() {
-        fail();
+        final String firstName = "src/test/java/ru/spbau/mit/testFile1.txt";
+        final String secondName = "src/test/java/ru/spbau/mit/testFile2.txt";
+        final List<String> filenames = asList(firstName, secondName);
+        assertEquals(
+                Collections.singletonList("abcdef"),
+                SecondPartTasks.findQuotes(filenames, "abc"));
+        assertEquals(
+                asList("abcdef", "defghi"),
+                SecondPartTasks.findQuotes(filenames, "de"));
+        assertEquals(
+                asList("defghi", "ghijkl", "ghijkl"),
+                SecondPartTasks.findQuotes(filenames, "hi"));
+        assertEquals(
+                Collections.emptyList(),
+                SecondPartTasks.findQuotes(filenames, "zyw"));
+        assertEquals(
+                Collections.emptyList(),
+                SecondPartTasks.findQuotes(Collections.emptyList(), ""));
     }
 
     @Test
     public void testPiDividedBy4() {
-        fail();
+        assertEquals(Math.PI / 4, SecondPartTasks.piDividedBy4(), 0.01);
     }
 
     @Test
     public void testFindPrinter() {
-        fail();
+        Map<String, List<String>> compositions = new HashMap<>();
+        compositions.put("AuthorA:37", asList("10_letters", "15_letters.....", "12_letters.."));
+        compositions.put("AuthorB:37", asList("11_letters.", "14_letters....", "12_letters.."));
+        compositions.put("AuthorC:38", asList("12_letters..", "16_letters......", "10_letters"));
+        assertEquals("AuthorC:38", SecondPartTasks.findPrinter(compositions));
+
+        Map<String, List<String>> emptyCompositions = new HashMap<>();
+        assertEquals(null, SecondPartTasks.findPrinter(emptyCompositions));
     }
 
     @Test
     public void testCalculateGlobalOrder() {
-        fail();
+        Map<String, Integer> ordersA = new HashMap<>();
+        ordersA.put("Bread", 3);
+        ordersA.put("Milk", 10);
+        ordersA.put("Whiskey", 1000);
+
+        Map<String, Integer> ordersB = new HashMap<>();
+        ordersB.put("Watermelon", 4);
+        ordersB.put("Bread", 4);
+        ordersB.put("Milk", 9);
+
+        Map<String, Integer> expected = new HashMap<>();
+        expected.put("Bread", 7);
+        expected.put("Milk", 19);
+        expected.put("Whiskey", 1000);
+        expected.put("Watermelon", 4);
+        assertMapEquals(expected, SecondPartTasks.calculateGlobalOrder(asList(ordersA, ordersB)));
+
+        assertTrue(SecondPartTasks.calculateGlobalOrder(Collections.emptyList()).isEmpty());
     }
+
+    private <K, V> void assertMapEquals(Map<K, V> mapA, Map<?, ?> mapB) {
+        assertEquals(mapA.size(), mapB.size());
+        for (Map.Entry<K, V> entry : mapA.entrySet()) {
+            assertTrue(mapB.containsKey(entry.getKey()));
+            assertEquals(entry.getValue(), mapB.get(entry.getKey()));
+        }
+    }
+
 }

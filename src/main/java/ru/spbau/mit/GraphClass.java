41fc043d2b0f474538d4ba07a7f045d99461043c
Timofey Prodanov <timofey.prodanov@gmail.com>
2016-04-19 15:32:32 +0300

    Injector 1

---
+++ src/main/java/ru/spbau/mit/GraphClass.java
@@ -0,0 +1,62 @@
+package ru.spbau.mit;
+
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GraphClass {
+
+    private final Class<?> clazz;
+    private Class<?> implementation = null;
+    private List<GraphClass> parents = new ArrayList<>();
+    private boolean ambiguous = false;
+    private boolean hasChildren = false;
+
+    public GraphClass(Class<?> clazz) {
+        this.clazz = clazz;
+    }
+
+    public void addParent(GraphClass newParent) {
+        newParent.hasChildren = true;
+        parents.add(newParent);
+    }
+
+    private void corrupt() {
+        ambiguous = true;
+        for (GraphClass parent : parents) {
+            parent.corrupt();
+        }
+    }
+
+    public boolean isAmbiguous() {
+        return ambiguous;
+    }
+
+    public boolean isFinalImplementation() {
+        return !(hasChildren || clazz.isInterface() || Modifier.isAbstract(clazz.getModifiers()));
+    }
+
+    public void spreadImplementation() {
+        implementation = clazz;
+        for (GraphClass parent : parents) {
+            parent.spreadImplementation(clazz);
+        }
+    }
+
+    public Class<?> getImplementation() {
+        return implementation;
+    }
+
+    private void spreadImplementation(Class<?> implementation) {
+        if (this.implementation != null) {
+            corrupt();
+            return;
+        }
+
+        this.implementation = implementation;
+        for (GraphClass parent : parents) {
+            parent.spreadImplementation(clazz);
+        }
+    }
+
+}
--- src/main/java/ru/spbau/mit/Injector.java
+++ src/main/java/ru/spbau/mit/Injector.java
@@ -1,9 +1,15 @@
 package ru.spbau.mit;
 
-import java.util.List;
+import java.lang.reflect.Constructor;
+import java.util.*;
 
 
 public final class Injector {
+
+    private static Map<Class<?>, GraphClass> graphNodes = new HashMap<>();
+    private static Map<Class<?>, Object> implementations = new HashMap<>();
+    private static Set<Class<?>> visited = new HashSet<>();
+
     private Injector() {
     }
 
@@ -12,6 +18,79 @@ public final class Injector {
      * `implementationClassNames` for concrete dependencies.
      */
     public static Object initialize(String rootClassName, List<String> implementationClassNames) throws Exception {
-        throw new IllegalStateException();
+        Class<?> root = Class.forName(rootClassName);
+        graphNodes.put(root, new GraphClass(root));
+        constructImplementationClasses(implementationClassNames);
+        return buildObject(root);
     }
+
+
+    private static void constructImplementationClasses(List<String> implementationClassNames)
+            throws ClassNotFoundException {
+        for (String className : implementationClassNames) {
+            Class<?> currentClass = Class.forName(className);
+
+            if (!graphNodes.containsKey(currentClass)) {
+                graphNodes.put(currentClass, new GraphClass(currentClass));
+            }
+            GraphClass currentNode = graphNodes.get(currentClass);
+
+            addParent(currentNode, currentClass.getSuperclass());
+            for (Class<?> parentInterface : currentClass.getInterfaces()) {
+                addParent(currentNode, parentInterface);
+            }
+        }
+
+        for (Map.Entry<Class<?>, GraphClass> entry : graphNodes.entrySet()) {
+            if (entry.getValue().isFinalImplementation()) {
+                entry.getValue().spreadImplementation();
+            }
+        }
+    }
+
+    private static void addParent(GraphClass currentNode, Class<?> parent) {
+        if (parent == null) {
+            return;
+        }
+        if (!graphNodes.containsKey(parent)) {
+            graphNodes.put(parent, new GraphClass(parent));
+        }
+        GraphClass superNode = graphNodes.get(parent);
+        currentNode.addParent(superNode);
+    }
+
+    private static Object buildObject(Class<?> clazz) throws Exception {
+        if (implementations.containsKey(clazz)) {
+            return implementations.get(clazz);
+        }
+
+        GraphClass node = graphNodes.get(clazz);
+        if (node.isAmbiguous()) {
+            throw new AmbiguousImplementationException();
+        }
+
+        if (node.getImplementation() == null) {
+            throw new ImplementationNotFoundException();
+        }
+
+        if (visited.contains(clazz)) {
+            throw new InjectionCycleException();
+        }
+        visited.add(clazz);
+
+        Class<?> implementationClass = node.getImplementation();
+        Constructor<?> constructor = implementationClass.getConstructors()[0];
+        Class<?>[] parameterTypes = constructor.getParameterTypes();
+        Object[] parameters = new Object[parameterTypes.length];
+        int i = 0;
+        for (Class<?> type : parameterTypes) {
+            parameters[i] = buildObject(type);
+            i += 1;
+        }
+
+        Object newObj = constructor.newInstance(parameters);
+        implementations.put(clazz, newObj);
+        return newObj;
+    }
+
 }
